4c4938e32ba40150e8fe70b1d72756ae7cfece37
Robin Roth <robin.roth1@students.fhnw.ch>
2023-10-05 13:51:41 +0200

    change to 4 spaces indentation

--- 02-lists/src/main/java/ch/fhnw/algd2/collections/list/linkedlist/MyLinkedList.java
+++ 02-lists/src/main/java/ch/fhnw/algd2/collections/list/linkedlist/MyLinkedList.java
@@ -5,135 +5,135 @@ import java.util.Arrays;
 import ch.fhnw.algd2.collections.list.MyAbstractList;
 
 public class MyLinkedList<E> extends MyAbstractList<E> {
-	private int size = 0;
-	private Node<E> first;
-	private Node<E> last;
+    private int size = 0;
+    private Node<E> first;
+    private Node<E> last;
 
-	@Override
-	public boolean add(E e) {
+    @Override
+    public boolean add(E e) {
 
-		Node<E> newNode = new Node<>(e);
+        Node<E> newNode = new Node<>(e);
 
-		if (size == 0) {
-			first = newNode;
-		} else {
-			last.next = newNode;
-		}
-		last = newNode;
-		size++;
-		return true;
-	}
+        if (size == 0) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+        }
+        last = newNode;
+        size++;
+        return true;
+    }
 
-	@Override
-	public boolean contains(Object o) {
-		// TODO: implement contains using while
-		Node<E> currentNode = first;
-		for (int i=0; i<size; i++) {
-			if (currentNode.elem.equals(o)) {
-				return true;
-			}
-			currentNode = currentNode.next;
-		}
-		return false;
-	}
+    @Override
+    public boolean contains(Object o) {
+        // TODO: implement contains using while
+        Node<E> currentNode = first;
+        for (int i=0; i<size; i++) {
+            if (currentNode.elem.equals(o)) {
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
 
-	@Override
-	public boolean remove(Object o) {
-		Node<E> current = first;
-		Node<E> prev = first;
-		while(current != null && !current.elem.equals(o)) {
-			prev = current;
-			current = current.next;
-		}
+    @Override
+    public boolean remove(Object o) {
+        Node<E> current = first;
+        Node<E> prev = first;
+        while(current != null && !current.elem.equals(o)) {
+            prev = current;
+            current = current.next;
+        }
 
-		// o not contained
-		if (current == null) return false;
+        // o not contained
+        if (current == null) return false;
 
-		// o is first
-		if (first == current) {
-			first = current.next;
+        // o is first
+        if (first == current) {
+            first = current.next;
 
-			// o is the only element of the list
-			if (size == 1) {
-				first = null;
-				last = null;
-				current.next = null;
-			}
-			size--;
-			return true;
-		}
+            // o is the only element of the list
+            if (size == 1) {
+                first = null;
+                last = null;
+                current.next = null;
+            }
+            size--;
+            return true;
+        }
 
-		// o is last
-		if (last == current) {
-			prev.next = null;
-			last = prev;
-			size--;
-			return true;
-		}
+        // o is last
+        if (last == current) {
+            prev.next = null;
+            last = prev;
+            size--;
+            return true;
+        }
 
-		// o is an element in the middle
-		prev.next = current.next;
-		current.next = null;
-		size--;
-		return true;
-	}
+        // o is an element in the middle
+        prev.next = current.next;
+        current.next = null;
+        size--;
+        return true;
+    }
 
-	@Override
-	public E get(int index) {
-		// TODO implement this operation (part D)
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public E get(int index) {
+        // TODO implement this operation (part D)
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void add(int index, E element) {
-		// TODO implement this operation (part D)
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void add(int index, E element) {
+        // TODO implement this operation (part D)
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public E remove(int index) {
-		// TODO implement this operation (part D)
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public E remove(int index) {
+        // TODO implement this operation (part D)
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public Object[] toArray() {
-		Object[] array = new Object[size];
-		int index = 0;
-		Node<E> current = first;
-		while (current != null) {
-			array[index++] = current.elem;
-			current = current.next;
-		}
-		return array;
-	}
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        int index = 0;
+        Node<E> current = first;
+        while (current != null) {
+            array[index++] = current.elem;
+            current = current.next;
+        }
+        return array;
+    }
 
-	@Override
-	public int size() {
-		return size;
-	}
+    @Override
+    public int size() {
+        return size;
+    }
 
-	private static class Node<E> {
-		private final E elem;
-		private Node<E> next;
+    private static class Node<E> {
+        private final E elem;
+        private Node<E> next;
 
-		private Node(E elem) {
-			this.elem = elem;
-		}
+        private Node(E elem) {
+            this.elem = elem;
+        }
 
-		private Node(E elem, Node<E> next) {
-			this.elem = elem;
-			this.next = next;
-		}
-	}
+        private Node(E elem, Node<E> next) {
+            this.elem = elem;
+            this.next = next;
+        }
+    }
 
-	public static void main(String[] args) {
-		MyLinkedList<Integer> list = new MyLinkedList<Integer>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		System.out.println(list.contains(2));
-		System.out.println(list.remove(new Integer(2)));
-		System.out.println(Arrays.toString(list.toArray()));
-	}
+    public static void main(String[] args) {
+        MyLinkedList<Integer> list = new MyLinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list.contains(2));
+        System.out.println(list.remove(new Integer(2)));
+        System.out.println(Arrays.toString(list.toArray()));
+    }
 }
